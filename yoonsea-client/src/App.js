94c84d4e397ba4de7ececef88188b94e4e25075a
EllaDev <ellawork23@gmail.com>
2022-04-11 19:22:58 +0900

    bug(윤은주): Set downgrade react, react-dom, react-scripts
     cause : conflict webpack5 and other library

--- yoonsea-client/src/App.js
+++ yoonsea-client/src/App.js
@@ -1,5 +1,5 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 import HomePage from './pages/HomePage';
@@ -7,14 +7,14 @@ import ExplorePage from './pages/ExplorePage';
 import CreatePage from './pages/CreatePage';
 import ProfilePage from './pages/ProfilePage';
 import './App.css';
+// import Web3 from 'web3';
 
 function App() {
   // eth, klaytn (팀원과 협의) => eth rospten
   // server, db가 들어갈 부분? => 안쓴다
   // ipfs에서 이미지를 가져오는데 시간이 많이 걸린다?
-
   return (
-    <BrowserRouter>
+    <div className="app">
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
@@ -23,7 +23,7 @@ function App() {
         <Route path="/profile" element={<ProfilePage />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </div>
   );
 }
 
