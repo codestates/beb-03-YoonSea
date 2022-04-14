e50c8c496230c440eca5a8c0ab306162f83ea789
EllaDev <ellawork23@gmail.com>
2022-04-14 15:32:19 +0900

    chore(윤은주): add the 'no-unused-vars'

--- yoonsea-client/.eslintrc.js
+++ yoonsea-client/.eslintrc.js
@@ -7,5 +7,6 @@ module.exports = {
   extends: ['react-app', 'eslint:recommended', 'plugin:prettier/recommended'],
   rules: {
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'no-unused-vars': 'warn',
   },
 };
